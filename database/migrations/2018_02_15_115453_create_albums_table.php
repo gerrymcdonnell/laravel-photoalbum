4ae2fb700d0e415bdd7706f2cbb75b25c88bc920
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-15 12:16:32 +0000

    adding fields to migrations and default data

--- database/migrations/2018_02_15_115453_create_albums_table.php
+++ database/migrations/2018_02_15_115453_create_albums_table.php
@@ -15,9 +15,42 @@ class CreateAlbumsTable extends Migration
     {
         Schema::create('albums', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('name');
+            $table->text('description');
+            $table->string('cover_image');
+
             $table->timestamps();
         });
+
+        $this->insertDefaultData();
     }
+
+
+    public function insertDefaultData(){
+        //setup default data
+        $rows = [
+            [
+            'name'=>'test album',
+            'description'=>'test album dewsc',
+            'cover_image'=>'my cover image here',
+
+            'created_at'=>date('Y-m-d G:i:s'),
+            'updated_at'=>date('Y-m-d G:i:s')
+            ],
+            [
+            'name'=>'test album2',
+            'description'=>'test album dewsc2',
+            'cover_image'=>'my cover image2 here',
+
+            'created_at'=>date('Y-m-d G:i:s'),
+            'updated_at'=>date('Y-m-d G:i:s')
+            ]
+        ];
+        // insert records
+        DB::table('albums')->insert($rows);
+    }
+
 
     /**
      * Reverse the migrations.
--- database/migrations/2018_02_15_115506_create_photos_table.php
+++ database/migrations/2018_02_15_115506_create_photos_table.php
@@ -15,9 +15,53 @@ class CreatePhotosTable extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->integer('album_id');
+            //filename
+            $table->string('photo');
+            $table->string('title');
+            $table->string('size');
+            $table->string('description');
+
             $table->timestamps();
         });
+
+        $this->insertDefaultData();
     }
+
+
+
+    public function insertDefaultData(){
+        //setup default data
+        $rows = [
+            [
+                'album_id'=>1,
+                'photo'=>'test album dewsc',
+                'title'=>'my cover image here',
+                'size'=>'234 kb',
+                'description'=>'my desc image here',
+
+                'created_at'=>date('Y-m-d G:i:s'),
+                'updated_at'=>date('Y-m-d G:i:s')
+            ],
+            [
+                'album_id'=>1,
+                'photo'=>'test album dewsc2',
+                'title'=>'my cover image here2',
+                'size'=>'834 kb',
+                'description'=>'my desc image2 here',
+
+                'created_at'=>date('Y-m-d G:i:s'),
+                'updated_at'=>date('Y-m-d G:i:s')
+            ],
+        ];
+        // insert records
+        DB::table('photos')->insert($rows);
+    }
+
+
+
+
 
     /**
      * Reverse the migrations.
